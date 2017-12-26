61ff0616ae49ff8894a403e30fdff3463bbb2bd7
Sarah Nooravi <snooravi17@gmail.com>
2017-12-26 13:37:06 -0800

    updated Chatterbot

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "LlzsA67oaAJb0VXVoHsPEAIwi"
+consumer_secret = "FqK2BTszDxMAUCRvuCYDbXReqwTixu2yyk35ODikepsmotBTPa"
+access_token = "913158636133801984-qOj7tKLMpDQaMYiE97ZYdu8OlF7nO9q"
+access_token_secret = "YwWNbpxqwZ8iFYvwQrvEzflt4CZTaj2Cnbn0VFZkTpTtJ"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,4 +16,23 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
-# CODE GOES HERE
+def TweetOut(tweet_number):
+    api.update_status(
+        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        tweet_number)
+
+
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+# Infinitely loop
+while(True):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
